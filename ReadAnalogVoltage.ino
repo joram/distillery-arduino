3a514a0001a5d87794bdbef84aa2c0782633ee57
John Oram <john.c.oram@gmail.com>
2016-05-21 16:53:19 -0700

    Update ReadAnalogVoltage.ino

--- ReadAnalogVoltage.ino
+++ ReadAnalogVoltage.ino
@@ -1,33 +1,40 @@
-int i = 0;
-int d = 5;
-const int max_poles = 5;
-const int num_sensors = 6;
-int values[num_sensors][max_poles];
 
+int value_i = 0;
+
+// a buffered history of values
+const int max_poles = 20;
+static const uint8_t pins[] = {A0, A1, A2, A3, A4, A5};
+int values[sizeof(pins)][max_poles];
+  
 int average(int* array, int length){
   int avg = 0;
   for(int j=0; j<length; j++){
     avg += array[j];
   }
-  return int(avg/sizeof(array));
+  return int(avg/length);
 }
 
+int safeAnalogRead(int pin)
+{
+  analogRead(pin);        // make an initial reading to set up the ADC
+  delay(1);               // let the ADC stabilize
+  return analogRead(pin); // toss the first reading and take one we will keep
+}
 
 void read_values(){
-  values[0][i] = analogRead(A0);
-  values[1][i] = analogRead(A1);
-  values[2][i] = analogRead(A2);
-  values[3][i] = analogRead(A3);
-  values[4][i] = analogRead(A4);
-  values[5][i] = analogRead(A5);
-  i++;
-  delay(d);
+  if(value_i >= max_poles){
+    value_i = 0;
+  }
+  for(int x=0; x<sizeof(pins); x++){
+    values[x][value_i] = safeAnalogRead(pins[x]);
+  }
+  value_i++;
 }
 
 String json_array(int* array, int length){
   String s = "[";
   if(length>0){
-    s += String(array[0]);
+    s += array[0];
     for(int x=1; x<length; x++){
       s += "," + String(array[x]);
     }
@@ -38,8 +45,12 @@ String json_array(int* array, int length){
 
 void print_averages(){
   String s = "{";
-  for(int j=0; j<num_sensors; j++){
-    s += "'A"+String(j)+"': "+String(average(values[j], max_poles))+",";
+  for(int j=0; j<sizeof(pins); j++){
+    int avg = average(values[j], max_poles);
+    s += "\"A"+String(j)+"\": "+String(avg);
+    if (j != sizeof(pins)-1){
+      s += ",";
+    }
   }
   s += "}";
   Serial.println(s);
@@ -47,9 +58,9 @@ void print_averages(){
 
 void print_values(){
   String s = "{";
-  for(int j=0; j<num_sensors; j++){
+  for(int j=0; j<sizeof(pins); j++){
     s += "\"A"+String(j)+"\": "+json_array(values[j], max_poles);
-    if (j != num_sensors-1){
+    if (j != sizeof(pins)-1){
       s += ",";
     }
   }
@@ -63,25 +74,24 @@ void setup() {
 }
 
 void loop() {
-  if(i >= max_poles){
-    print_values();
-    i = 0;
-  }
   read_values();
-
 
   if (Serial.available() > 0) {
 
     String s = Serial.readString();
-//    if(s == "ping"){
-//      Serial.println("pong");
-//    } else if(s == "values"){
-//      print_values();
-//    } else if(s == "averages"){
-//      print_averages();
-//    } else {
-//      Serial.println("unknown command: '"+s+"', valid commands are: [values, averages]");
-//    }
+    if(s == "ping"){
+      Serial.println("pong");
+    } 
+    else if(s == "values"){
+      print_values();
+    } 
+    else if(s == "averages"){
+      print_averages();
+    } 
+    else {
+      Serial.println("unknown command: '"+s+"', valid commands are: [values, averages]");
+    }
   }
 }
+
 
